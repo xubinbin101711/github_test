195fa165fb38e7c2a2c8b0e23c168e2becd2bb2f
your name <your_email@youremail.com>
2019-09-23 15:31:18 +0800

    modeified test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: fengjunhui 
+	#	 Email		: 18883765905@163.com 
+	#	 Created	: Mon 23 Sep 2019 03:28:33 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("dsajd");
+    return 0;
+}
