face6f2210257f75460d34042763af1b0fb46516
xubinbin1994 <1059500770@qqcom>
2019-09-23 21:47:50 +0800

    add test.c

--- test.c
+++ test.c
@@ -2,14 +2,12 @@
 	#	 FileName	: test.c
 	#	 Author		: fengjunhui 
 	#	 Email		: 18883765905@163.com 
-	#	 Created	: Mon 23 Sep 2019 03:28:33 PM CST
+	#	 Created	: Mon 23 Sep 2019 09:28:22 PM CST
  ************************************************************************/
 
 #include<stdio.h>
 
 int main(int argc, const char *argv[])
 {
-	printf("dsajd");
-	printf("adhkas");
     return 0;
 }
